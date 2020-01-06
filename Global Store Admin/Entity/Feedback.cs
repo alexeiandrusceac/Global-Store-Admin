753951197bc098ed9211cf62fc6351be13bf2170
alexei andrusceac <alexeiandrusceac@hotmail.com>
2020-01-06 11:55:47 +0200

    Add project files.

---
+++ Global Store Admin/Entity/Feedback.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Global_Store_Admin.Entity
+{
+   public class Feedback
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public Feedback() { }
+
+        
+    }
+}
---
+++ Global Store Admin/WebInteraction/WebInteraction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Global_Store_Admin.Entity;
+namespace Global_Store_Admin.WebInteraction
+{
+    public class WebInteraction
+    {
+        public string BASIC_WEB = "https://localhost:";
+        public string GETFEEDBACKS = "getFeedback";
+
+        public WebInteraction()
+        { 
+        
+        }
+        public List<Feedback> getListFeedback()
+        {
+            List<Feedback> listOFFeedbacks = new List<Feedback>();
+
+
+            return listOFFeedbacks;
+        }
+    }
+}
